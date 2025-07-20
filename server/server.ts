bc45125f640fd01b0366b77ff962c1b566325dfb
Indrajeet1256 <Indrajeetgiri007@gmail.com>
2025-07-20 10:17:28 +0530

    final commit

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,6 @@
 import { config } from "dotenv";
 import express from "express";
-import { clerkMiddleware } from "@clerk/express";
+import { clerkMiddleware, requireAuth } from "@clerk/express";
 import { serve } from "inngest/express";
 import { client } from "./src/inngest/index.ts";
 import { functions } from "./src/inngest/functions.ts";
@@ -11,10 +11,15 @@ const PORT = parseInt(process.env.PORT!);
 const app = express();
 
 app.use(clerkMiddleware());
+app.use(requireAuth());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/inngest", serve({ client, functions }));
+
+app.get("/", (req, res) => {
+	res.send("We Are Live");
+});
 
 app.listen(PORT, () => {
 	console.log("Server running on port:", PORT);
